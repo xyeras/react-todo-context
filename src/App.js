16a4d868ce790c95fabefb34e5a58a94b8801d39
xyeras <saika_fujimoto@mail.fresnostate.edu>
2021-04-27 15:38:30 -0700

    feat(base-components): created TodoInput, TodoList, and TodoItem as components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList';
+import TodoItem from './components/TodoItem';
 
 const App = () => {
   return (
     <div className="container my-5">
-
+        <TodoInput />
+        <TodoList />
+        <TodoItem />
     </div>
   );
 }
---
+++ src/components/TodoItem.js
@@ -0,0 +1,9 @@
+const TodoItem = () => {
+    return (
+      <div>
+          This is TodoItem!
+      </div>
+    );
+  }
+  
+  export default TodoItem;
