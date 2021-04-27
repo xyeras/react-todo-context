add23cf4015b15c71f1c2503dbd0151ed3d6c3fc
xyeras <saika_fujimoto@mail.fresnostate.edu>
2021-04-27 16:53:31 -0700

    feat(TodoList): created TodoList component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,45 @@
-const TodoList = () => {
+const TodoList = ({ todos, removeTodo, setTodo }) => {
+
     return (
+  
       <div>
-          This is TodoList!
+  
+            {todos.map(todo => {
+              return (
+              
+              <div className="list-group">
+  
+                <div className="list-group-item list-group-item-action">
+  
+                  <div className="d-flex w-100 justify-content-between">
+                    <h5 className="mb-1">{todo.name}</h5>
+                    {/* <small class="text-muted">Quest ID #{todo.id}</small> */}
+                    <small className="text-muted">Difficulty: {todo.difficulty}</small>
+                  </div>
+  
+                  <p className="mb-1">{todo.description}</p>
+  
+                  {/* <small class="text-muted">Difficulty: {todo.difficulty}</small> */}
+                  <small className="text-muted">Quest ID #{todo.id}</small>
+  
+                  </div>
+  
+                  <button
+                    className='btn btn-success mb-2'
+                    onClick={() => removeTodo(todo.id)}>
+                    Complete quest
+                  </button>
+  
+              </div>
+              
+  
+              );
+            })}
+  
       </div>
+  
     );
-  }
+  };
   
   export default TodoList;
+  
